f4056f0d40a73f384b379378b85ce7a8d3248d20
IlyasArrafii <zainularrafii@gmail.cm>
2024-03-13 13:17:50 +0700

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallege from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallege tittle="Easy" targetTime={1}/>
+        <TimerChallege tittle="Not Easy" targetTime={5}/>
+        <TimerChallege tittle="Getting Tough" targetTime={10}/>
+        <TimerChallege tittle="Pros Only" targetTime={15}/>
+      </div>
     </>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(e) {
-    setSubmitted(false);
-    setEnteredPlayerName(e.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+    console.log('🚀 ~ handleClick ~ value:', playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
